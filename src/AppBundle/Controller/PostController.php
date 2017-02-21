10d1dda0604de5003a8bddb5da4c9b2eb48111e4
julienbinet <julien.binet.27@gmail.com>
2017-02-21 19:26:20 +0100

    Add Comment Form on Post Page

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Category;
+use AppBundle\Entity\Comment;
 use AppBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -17,17 +18,38 @@ class PostController extends Controller
      * Finds and displays a post entity.
      *
      * @Route("post/{id}", name="post_show")
-     * @Method("GET")
+     * @Method({"GET", "POST"})
      */
-    public function showAction(Post $post)
+    public function showAction(Request $request, Post $post)
     {
 
         $em = $this->getDoctrine()->getManager();
+
+
+        $comment = new Comment();
+        $form = $this->createForm('AppBundle\Form\CommentType', $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $comment->setPost($post);
+
+            $em->persist($comment);
+
+            $em->flush($comment);
+
+            $this->get('session')->getFlashBag()->add('success', "Le commentaire a bien été créé");
+
+//            return $this->redirectToRoute('admin_post_show', array('id' => $post->getId()));
+        }
+
         $comments = $em->getRepository('AppBundle:Comment')->CommentsByPost($post->getId());
 
         return $this->render('public/post.html.twig', array(
             'post' => $post,
-            'comments' => $comments
+            'comments' => $comments,
+            'form' => $form->createView(),
         ));
     }
 
